b3ad866dd76cd4ad1572d96fe6c91d96b3100726
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-12 09:28:41 +0100

    First exercise

---
+++ exercise 1/assets/script.js
@@ -0,0 +1,21 @@
+let main = document.querySelector("main");
+
+let wordToDisplay = "prout";
+
+let i = 0;
+let timeout = 1000;
+function wouhou(){
+   
+    if ( i < wordToDisplay.length){
+      main.innerHTML += wordToDisplay.charAt(i);  
+      i++; 
+      setTimeout(wouhou, timeout);
+    }
+    }
+
+    clearTimeout(timeout);
+
+    wouhou();
+
+
+
