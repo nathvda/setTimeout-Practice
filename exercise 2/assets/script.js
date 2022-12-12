008e4dcef6604a9ec7f9b18c3f93286c6af51a35
Nathalie Vanden Abeele <nath.vandenabeele@gmail.com>
2022-12-12 09:40:30 +0100

    Exercise 2

---
+++ exercise 2/assets/script.js
@@ -0,0 +1,26 @@
+let main = document.querySelector("main");
+let secondblock = document.getElementById("seconds");
+let minutesblock = document.getElementById("minutes");
+
+let seconds = 0; 
+let minutes = 0;
+
+function displaySeconds(){ 
+    secondblock.innerHTML = seconds;
+    seconds++;
+}
+
+function displayMinutes(){ 
+    minutes = Math.floor((seconds - 1) / 60);
+    if (minute < 1) {
+    }
+    else if (minutes < 2){
+    minutesblock.innerHTML = minutes + " minutes has passed";
+} else { minutesblock.innerHTML = minutes + " minutes have passed";
+}
+
+}
+
+setInterval(displaySeconds, 1000);
+setInterval(displayMinutes, 1000);
+
